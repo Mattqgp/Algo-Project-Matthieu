318b0f4b4a9390562dca700d6b31d6ed13d344a0
minhdang210799 <98717844+minhdang210799@users.noreply.github.com>
2024-03-30 16:35:34 +0700

    terrain + lighting

--- Assets/Scripts/SpawnBullets.cs
+++ Assets/Scripts/SpawnBullets.cs
@@ -16,7 +16,7 @@ public class SpawnBullets : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
             Instantiate(Bullet, ShootStart.position, ShootStart.rotation);
         }
